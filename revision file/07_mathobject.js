b4d1fb761a7eba51a945c490d0aa09da5cbe7824
Hemant kumar gola <149658773+hemantkumargola@users.noreply.github.com>
2026-01-24 17:16:46 +0530

    math property
    math property function

---
+++ revision file/07_mathobject.js
@@ -0,0 +1,33 @@
+// math object in js 
+// Math object JavaScript ka built-in object hota hai jo mathematical 
+// operations perform karne ke kaam aata hai.
+
+// // example 1
+// let x = Math.PI;
+// console.log(x);
+
+
+
+
+
+
+
+// 👉Math.abs() 
+
+//  Absolute value ka matlab hota hai number ka
+//  positive value (sign hata deta hai).
+// example
+// let profitLoss = -250;
+// console.log(Math.abs(profitLoss)); // 250
+
+
+
+
+
+
+
+
+
+
+// Math.round() JavaScript ka built-in method hai jo number ko nearest intege
+// r (sabse kareeb poora number) me round kar deta hai.
